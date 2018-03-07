d190cff1790e55866f4349dc496158f94cc82b09
Bartosz Śliwa <daszek212@gmail.com>
2018-03-07 19:18:33 +0100

    Initial commit

---
+++ PHP/head.php
@@ -0,0 +1,9 @@
+<?php
+echo '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="pl" lang="pl">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<meta name="Description" content="Moja strona" />
+	<title>[Bartosz Śliwa]</title>
+</head>';
+?>
---
+++ PHP/wybor.php
@@ -0,0 +1,11 @@
+<?php
+echo '<div id="menu">
+	<div class="Odnosniki">
+        <h3>Odnosniki</h3>
+            <a href="nowy.php">Zakładanie bloga</a>
+            <a href="listablogow.php">Lista blogów</a>
+            <a href="wpis.php">Szybki wpis</a>
+            <a href="komunikator.php">Komunikator</a>
+    </div>
+</div>';
+?>
